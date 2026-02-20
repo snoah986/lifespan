ae9de3ce2cd1122d69e9269e6d22707377eacc71
snoah986 <snoah986@gmail.com>
2026-02-20 15:27:16 +0000

    Create character.js file with CharacterTemplate structure

---
+++ src/engine/character.js
@@ -0,0 +1,37 @@
+const CharacterTemplate = {
+  identity: {
+    name: '',
+    birthYear: 0,
+    birthCountry: '',
+    socialClass: '',
+    age: 0,
+    alive: true,
+    gender: '',
+    sexuality: '',
+    ethnicity: '',
+  },
+  cognitive: {
+    intelligence: 10, memory: 10, creativity: 10,
+    analytical: 10, wisdom: 10, languageAptitude: 10, mathAptitude: 10,
+  },
+  social: {
+    charisma: 10, empathy: 10, assertiveness: 10,
+    manipulation: 10, trustworthiness: 10, humour: 10,
+    socialAwareness: 10, networking: 10, publicSpeaking: 10,
+  },
+  psychological: {
+    ambition: 10, resilience: 10, emotionalStability: 10,
+    impulseControl: 10, courage: 10, integrity: 10,
+    adaptability: 10, stressTolerance: 10, forgiveness: 10, paranoia: 5,
+  },
+  physical: {
+    fitness: 10, strength: 10, stamina: 10,
+    appearance: 10, coordination: 10, healthConstitution: 10,
+  },
+  hidden: {
+    addictionVulnerability: 0, criminalTendency: 0,
+    mentalHealthFragility: 0, loyaltyThreshold: 10, darkTriadPotential: 0,
+  },
+  lifeStats: {
+    health: 80, happiness: 60, wealth​
+}
